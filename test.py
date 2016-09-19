5474f7d4dd1c1a56f286c5654fb738716b1a61c2
acflorea <acflorea@gmail.com>
2016-09-19 20:31:09 +0300

    Export to csv. Configurable database

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from dbloader import getTextForDictionary, bugDicoToFullText, getBugDetails
+import pandas as pd
 
 # retrieve descriptions from db
 # bug_descs = getTextForDictionary(1000)
@@ -7,7 +8,33 @@ from dbloader import getTextForDictionary, bugDicoToFullText, getBugDetails
 
 # print fulltextdesc
 
-bug_dataframe = getBugDetails('netbeansbugs')
-# bug_dataframe = getBugDetails('eclipsebugs')
-# bug_dataframe = getBugDetails('firefoxbugs_new')
-print bug_dataframe.head(10)
+# db = 'netbeansbugs'
+# db = 'eclipsebugs'
+db = 'firefoxbugs_new'
+
+
+#  ==========================================================
+def fetchAndSaveDataframe(db):
+    print("Database is %s" % db)
+    print("Fetching data")
+
+    bug_dataframe = getBugDetails(db)
+
+    print("Data frame fetched, here's an extract")
+    print bug_dataframe.head(10)
+
+    print('Saving dataframe')
+    bug_dataframe.to_csv("./%s.csv" % db, encoding='utf-8')
+
+    return bug_dataframe
+
+
+def loadDataframe(db):
+    bug_dataframe = pd.read_csv("./%s.csv" % db, encoding='utf-8', index_col=0)
+    return bug_dataframe
+
+
+# fetchAndSaveDataframe(db)
+bug_dataframe = loadDataframe(db)
+print(bug_dataframe.shape)
+print(bug_dataframe.head())
