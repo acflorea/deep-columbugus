6cfc114e40ae3e04adaa0455ff8376c29efe2758
acflorea <acflorea@gmail.com>
2016-09-19 21:55:56 +0300

    Split data, train ans evaluate linear SVC

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 from dbloader import getTextForDictionary, bugDicoToFullText, getBugDetails
 import pandas as pd
+from sklearn.cross_validation import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from time import time
+from sklearn.svm import LinearSVC
+from sklearn import metrics
+from sklearn.utils.extmath import density
 
 # retrieve descriptions from db
 # bug_descs = getTextForDictionary(1000)
@@ -8,9 +14,13 @@ import pandas as pd
 
 # print fulltextdesc
 
-# db = 'netbeansbugs'
+seed = 42
+
+db = 'netbeansbugs'
+
+
 # db = 'eclipsebugs'
-db = 'firefoxbugs_new'
+# db = 'firefoxbugs_new'
 
 
 #  ==========================================================
@@ -29,12 +39,60 @@ def fetchAndSaveDataframe(db):
     return bug_dataframe
 
 
+#  ==========================================================
 def loadDataframe(db):
     bug_dataframe = pd.read_csv("./%s.csv" % db, encoding='utf-8', index_col=0)
     return bug_dataframe
 
 
-# fetchAndSaveDataframe(db)
+###############################################################################
+# Benchmark classifiers
+def benchmark(clf):
+    print('_' * 80)
+    print("Training: ")
+    print(clf)
+    t0 = time()
+    clf.fit(X_train, y_train)
+    train_time = time() - t0
+    print("train time: %0.3fs" % train_time)
+
+    t0 = time()
+    pred = clf.predict(X_test)
+    test_time = time() - t0
+    print("test time:  %0.3fs" % test_time)
+
+    score = metrics.accuracy_score(y_test, pred)
+    print("accuracy:   %0.3f" % score)
+
+    print()
+
+    print("classification report:")
+    print(metrics.classification_report(y_test, pred))
+    print("confusion matrix:")
+    print(metrics.confusion_matrix(y_test, pred))
+
+
+print()
+clf_descr = str(clf).split('(')[0]
+return clf_descr, score, train_time, test_time  # fetchAndSaveDataframe(db)
 bug_dataframe = loadDataframe(db)
-print(bug_dataframe.shape)
-print(bug_dataframe.head())
+print("Dataframe loaded %s" % str(bug_dataframe.shape))
+
+print("Test/Train split")
+train, test = train_test_split(bug_dataframe, train_size=0.8, random_state=seed)
+y_train, y_test = train.assigned_to, test.assigned_to
+
+print("Train is %s" % str(train.shape))
+print("Test is %s" % str(test.shape))
+
+labels = train.assigned_to
+vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
+                             stop_words='english')
+
+X_train = vectorizer.fit_transform(train.text.astype('U'))
+X_test = vectorizer.transform(test.text.astype('U'))
+
+print("Vectorized!")
+
+results = benchmark(LinearSVC(loss='l2', penalty='squared_hinge',
+                              dual=False, tol=1e-3))
